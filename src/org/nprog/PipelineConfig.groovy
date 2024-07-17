53860278432378323e3e7e76f15c8568673b7d63
kornzysiek <emai@dummy.com>
2024-07-17 19:45:02 +0000

    WIP

--- src/org/nprog/PipelineConfig.groovy
+++ src/org/nprog/PipelineConfig.groovy
@@ -7,7 +7,7 @@ class PipelineConfig implements Serializable {
 
     PipelineConfig(Map params, def steps) {
         def yamlConfig = loadYamlConfig(steps)
-        this.config = mergeMaps(yamlConfig, params)
+        this.config = mergeMaps(yamlConfig, params.appendStages ? params : overrideStages(params))
     }
 
     def getStages() {
@@ -15,26 +15,39 @@ class PipelineConfig implements Serializable {
     }
 
     @NonCPS
-    String toString() {
-        return config.toString()
+    private Map overrideStages(Map params) {
+        def result = params.clone()
+        if (params.containsKey('stages')) {
+            result.stages = params.stages
+        }
+        return result
     }
 
     @NonCPS
     private Map loadYamlConfig(def steps) {
-        if (steps) {
-            def yaml = new org.yaml.snakeyaml.Yaml()
-            def configText = steps.libraryResource('advanced_pipeline_config.yaml')
-            return yaml.load(configText)
-        } else {
-            return [:]
-        }
+        def yaml = new org.yaml.snakeyaml.Yaml()
+        def configText = steps.libraryResource('advanced_pipeline_config.yaml')
+        return yaml.load(configText)
     }
 
     @NonCPS
     private Map mergeMaps(Map... maps) {
         Map result = [:]
         maps.each { map ->
-            result.putAll(map)
+            result = mergeMapRecursive(result, map)
+        }
+        return result
+    }
+
+    @NonCPS
+    private Map mergeMapRecursive(Map base, Map override) {
+        Map result = new HashMap(base)
+        override.each { key, value ->
+            if (base[key] instanceof Map && value instanceof Map) {
+                result[key] = mergeMapRecursive(base[key], value)
+            } else {
+                result[key] = value
+            }
         }
         return result
     }
