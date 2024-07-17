5ee46a068d8cb8b699ad1c3c7ef77b0af4d14a2a
kornzysiek <emai@dummy.com>
2024-07-17 18:24:44 +0000

    WIP

--- src/org/nprog/Logger.groovy
+++ src/org/nprog/Logger.groovy
@@ -1,30 +1,28 @@
 package org.nprog
 
 class Logger {
-
     static final String ANSI_RESET = "\u001B[0m"
     static final String ANSI_GREEN = "\u001B[32m"
     static final String ANSI_RED = "\u001B[31m"
     static final String ANSI_BLUE = "\u001B[34m"
+    
+    boolean debugMode = false
 
-    static boolean debugMode = false
-
-    static void info(String message) {
+    void info(String message) {
         println("${ANSI_GREEN}INFO: ${message}${ANSI_RESET}")
     }
 
-    static void error(String message) {
+    void error(String message) {
         println("${ANSI_RED}ERROR: ${message}${ANSI_RESET}")
     }
 
-    static void debug(String message) {
+    void debug(String message) {
         if (debugMode) {
             println("${ANSI_BLUE}DEBUG: ${message}${ANSI_RESET}")
         }
     }
 
-    static def enableDebug() {
+    void enableDebug() {
         debugMode = true
     }
-
 }
