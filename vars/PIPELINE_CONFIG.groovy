156508e2e651b90b3b2d12bf08efe8cf91110ee6
kornzysiek <emai@dummy.com>
2024-07-18 18:38:33 +0000

    WIP

--- vars/PIPELINE_CONFIG.groovy
+++ vars/PIPELINE_CONFIG.groovy
@@ -1,7 +1,7 @@
 class PIPELINE_CONFIG {
-    static final DEFAULT_AGENT = 'agent'
-    static final SONAR_TOOL = 'SonarQube'
-    static final DEBUG_MODE_DEFAULT = false
-    static final DOCKER_TAG_FORMAT = { build_id, commit -> "${build_id}.${commit.take(7)}" }
-    static final SELENIUM_JOB = 'selenium'
+    DEFAULT_AGENT = 'agent'
+    SONAR_TOOL = 'SonarQube'
+    DEBUG_MODE_DEFAULT = false
+    DOCKER_TAG_FORMAT = { build_id, commit -> "${build_id}.${commit.take(7)}" }
+    SELENIUM_JOB = 'selenium'
 }
